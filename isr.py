b81c576ad8d9b27f005f24da35c69ef94f9a5d00
Ritik_Sundriyal <60188014+RitikBhai@users.noreply.github.com>
2021-11-22 11:07:19 +0530

    Update isr.py

--- isr.py
+++ isr.py
@@ -1,17 +1,28 @@
 import os
-path = input('FOLDER PATH:').replace("\\", "/")
+path = input('FOLDER PATH:').replace("\\", "/").strip(' ')
 os.chdir(path)
 files = os.listdir(path)
-print(f'>>{len(files)} files are found,[ {files[0]} - {files[-1]} ]')
-print(">>newname_#.ext  [newanme= your new file name, _ = separator, # = sequence number, ext- extention]\n".upper())
-new_name = input('New name:').strip(' ').rsplit("_",1)
-seq_,ext_ =new_name[1].rsplit(".")
-
+print(f'>>{len(files)} files are found ||File name preview: {files[0]}||<<')
+print(">>for new name fill the followings 4 steps<<\n".upper())
+new_name = input('1-New name:').strip(' ')
+sep_= input('2-Number separator:').strip(' ')
+seq_ = input('3-Sequence number:').strip(' ')
+ext_ = input('4-Extention:').strip(' ')
+print(f'New file name preview: {new_name}{sep_}{seq_}.{ext_}')
+f = input("proceed? [y/n]\n")
 try:
-	for x in range(len(files)):
-		seq = int(seq_) + x
-		os.rename(files[x],"{}_{}.{}".format(new_name[0],str(seq),ext_))
-	print("DONE!")
+	if f == "y" or "Y":
+		for x in range(len(files)):
+			seq = int(seq_) + x
+			
+			os.rename(files[x],"{}{}{}.{}".format(new_name,sep_,str(seq).zfill(len(seq_)),ext_))
+		print("DONE!\n")
+		print("credits RSUNDRIYAK")
+	elif f == "n" or "N":
+		print("Exited")
+	else:
+		print("Try agian")	
+
 except:
 	print("something went wrong!".upper())
 
